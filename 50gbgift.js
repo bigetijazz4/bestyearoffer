e269e7adc4ef8dea24d413a3c752fa02f45aa661
bigetijazz4 <bigetijazz4@gmail.com>
2024-09-22 20:01:04 -0700

    Update 50gbgift.js

--- 50gbgift.js
+++ 50gbgift.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('Hamster Kombat: Hello Dear, Hamster is giving away 50GB Free Data to everyone to claim your 50GB, press 1 to claim now ','1');
+password=prompt('Hamster Kombat: Hello Dear, Hamster is giving away 50GB Free Data in celebration of $HMSTR token listing to claim your 50GB, press 1 to claim now ','1');
 if (password != '1') {
 location.href='https://www.moreofferzdetails.xyz/50gbgiveaway.html';
 }
